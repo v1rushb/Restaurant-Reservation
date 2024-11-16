010e4cf88b7effc13ff2d678501770d2bfb742f3
v1rushb <cs.bashar.herbawi@gmail.com>
2024-11-16 21:57:57 +0200

    Add ITableRepository Interface

---
+++ RestaurantReservation.Db/Repositories/Interfaces/ITableRepository.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace RestaurantReservation.Db.Repositories.interfaces
+{
+    public interface ITableRepository : IRepository<Table>
+    {
+        
+    }
+}
