13307350fd88765ff34fe0fd57d025948cbfee0e
v1rushb <cs.bashar.herbawi@gmail.com>
2024-11-16 21:59:39 +0200

    Add IUserRepository Interface

---
+++ RestaurantReservation.Db/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,10 @@
+using RestaurantReservation.Db.Entities;
+using RestaurantReservation.Db.Repositories.interfaces;
+
+namespace RestaurantReservation.Db.Repositories
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        
+    }
+}
