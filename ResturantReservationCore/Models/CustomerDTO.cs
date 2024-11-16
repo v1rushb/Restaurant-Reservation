4a91e90f3d7b1d2c169a039a5ef752535dae3763
v1rushb <cs.bashar.herbawi@gmail.com>
2024-11-16 20:45:30 +0200

    Create Customer DTO

---
+++ ResturantReservationCore/Models/CustomerDTO.cs
@@ -0,0 +1,9 @@
+namespace RestaurantReservationCore.DTO {
+    public class CustomerDTO {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
