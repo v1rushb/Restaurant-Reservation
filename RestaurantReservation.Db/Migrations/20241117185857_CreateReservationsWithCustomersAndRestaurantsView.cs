33786456edede406f20dc2b5c8c3d5e97bbf74ce
v1rushb <cs.bashar.herbawi@gmail.com>
2024-11-17 21:05:02 +0200

    Add Migrations for ReservationsWithCustomersAndRestaurant View

---
+++ RestaurantReservation.Db/Migrations/20241117185857_CreateReservationsWithCustomersAndRestaurantsView.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace RestaurantReservation.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateReservationsWithCustomersAndRestaurantsView : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 1,
+                column: "OrderDate",
+                value: new DateTime(2024, 11, 17, 2, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 2,
+                column: "OrderDate",
+                value: new DateTime(2024, 11, 17, 4, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 3,
+                column: "OrderDate",
+                value: new DateTime(2024, 11, 17, 6, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 4,
+                column: "OrderDate",
+                value: new DateTime(2024, 11, 17, 8, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 5,
+                column: "OrderDate",
+                value: new DateTime(2024, 11, 17, 10, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "ReservationId",
+                keyValue: 1,
+                column: "ReservationDate",
+                value: new DateTime(2024, 11, 17, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "ReservationId",
+                keyValue: 2,
+                column: "ReservationDate",
+                value: new DateTime(2024, 11, 17, 1, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "ReservationId",
+                keyValue: 3,
+                column: "ReservationDate",
+                value: new DateTime(2024, 11, 17, 2, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "ReservationId",
+                keyValue: 4,
+                column: "ReservationDate",
+                value: new DateTime(2024, 11, 17, 3, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "ReservationId",
+                keyValue: 5,
+                column: "ReservationDate",
+                value: new DateTime(2024, 11, 17, 5, 0, 0, 0, DateTimeKind.Local));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 1,
+                column: "OrderDate",
+                value: new DateTime(2024, 11, 16, 2, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 2,
+                column: "OrderDate",
+                value: new DateTime(2024, 11, 16, 4, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 3,
+                column: "OrderDate",
+                value: new DateTime(2024, 11, 16, 6, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 4,
+                column: "OrderDate",
+                value: new DateTime(2024, 11, 16, 8, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 5,
+                column: "OrderDate",
+                value: new DateTime(2024, 11, 16, 10, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "ReservationId",
+                keyValue: 1,
+                column: "ReservationDate",
+                value: new DateTime(2024, 11, 16, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "ReservationId",
+                keyValue: 2,
+                column: "ReservationDate",
+                value: new DateTime(2024, 11, 16, 1, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "ReservationId",
+                keyValue: 3,
+                column: "ReservationDate",
+                value: new DateTime(2024, 11, 16, 2, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "ReservationId",
+                keyValue: 4,
+                column: "ReservationDate",
+                value: new DateTime(2024, 11, 16, 3, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "ReservationId",
+                keyValue: 5,
+                column: "ReservationDate",
+                value: new DateTime(2024, 11, 16, 5, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "UserId", "FirstName", "LastName", "Password", "Username" },
+                values: new object[,]
+                {
+                    { 1, "Ahmad", "Suleiman", "password123", "ahmad_s" },
+                    { 2, "Omar", "Yassin", "password123", "omar_y" },
+                    { 3, "Yousef", "Khatib", "password123", "yousef_k" },
+                    { 4, "Ali", "Zayed", "password123", "ali_z" },
+                    { 5, "Omar", "Herbawi", "password123", "omar_h" }
+                });
+        }
+    }
+}
