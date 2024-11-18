d71f23600a0926171b22eac45a991f88e3da5e8a
v1rushb <cs.bashar.herbawi@gmail.com>
2024-11-18 15:29:07 +0200

    Add RestaurantRevenue Migratiion

--- RestaurantReservation.Db/Migrations/20241118132045_RestaurantRevenueFunction.cs
+++ RestaurantReservation.Db/Migrations/20241118132045_RestaurantRevenueFunction.cs
@@ -10,7 +10,21 @@ namespace RestaurantReservation.Db.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.Sql(@"
+                CREATE FUNCTION fn_RestaurantRevenue (@restaurantId INT)
+                RETURNS DECIMAL(18, 2)
+                AS
+                BEGIN
+                    RETURN (
+                        SELECT COALESCE(SUM(mi.Price * oi.Quantity), 0)
+                        FROM Reservations r
+                        JOIN Orders o ON r.ReservationId = o.ReservationId
+                        JOIN OrderItems oi ON o.OrderId = oi.OrderId
+                        JOIN MenuItems mi ON oi.MenuItemId = mi.MenuItemId
+                        WHERE r.RestaurantId = @restaurantId
+                    )
+                END
+            ");
         }
 
         /// <inheritdoc />
