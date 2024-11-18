95673a755bfd6349ef924fc47f1d70e2465ec3a2
v1rushb <cs.bashar.herbawi@gmail.com>
2024-11-18 14:39:16 +0200

    Add EmployeeRestaurantDetailsView Migration

---
+++ RestaurantReservation.Db/Migrations/20241118123607_EmployeeRestaurantDetails.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantReservation.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class EmployeeRestaurantDetails : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                CREATE VIEW EmployeeRestaurantDetails AS
+                SELECT 
+                    e.employee_id,
+                    e.first_name,
+                    e.last_name,
+                    e.position,
+                    e.restaurant_id,
+                    res.name AS restaurant_name,
+                    res.address AS restaurant_address,
+                    res.phone_number AS restaurant_phone
+                FROM Employees e
+                INNER JOIN Restaurants res ON e.restaurant_id = res.restaurant_id;
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DROP VIEW IF EXISTS EmployeeRestaurantDetails;"
+            );
+        }
+    }
+}
