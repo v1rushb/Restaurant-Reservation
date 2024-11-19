e827570db08b96c21249352ccae4d71541f7ab90
v1rushb <cs.bashar.herbawi@gmail.com>
2024-11-19 15:15:48 +0200

    Reset RestaurantReservationCore structure

--- RestaurantReservation.Db/Repositories/Interfaces/ICustomerRepository.cs
+++ RestaurantReservation.Db/Repositories/Interfaces/ICustomerRepository.cs
@@ -2,7 +2,7 @@ using RestaurantReservation.Db.Entities;
 
 namespace RestaurantReservation.Db.Repositories.interfaces {
     public interface ICustomerRepository : IRepository<Customer>
-    {
+    {        
         Task<List<Customer>> GetCustomersWithPartySizeGreaterThanValueAsync(int value);
     }   
 }
