160dcab01fee45615dd3af4dc0e1a9e9829a2549
v1rushb <cs.bashar.herbawi@gmail.com>
2024-11-19 15:57:22 +0200

    Add Table Service

---
+++ ResturantReservationCore/Service/TableService.cs
@@ -0,0 +1,40 @@
+using RestaurantReservation.Db.Entities;
+using RestaurantReservation.Db.Repositories.interfaces;
+using RestaurantReservationCore.Service.Interfaces;
+
+namespace RestaurantReservationCore.Service
+{
+    public class TableService : ITableService
+    {
+        private readonly ITableRepository _tableRepository;
+
+        public TableService(ITableRepository tableRepository)
+        {
+            _tableRepository = tableRepository;
+        }
+
+        public async Task<int> CreateAsync(Table newTable)
+        {
+            return await _tableRepository.CreateAsync(newTable);
+        }
+        public async Task DeleteAsync(int tableId)
+        {
+            await _tableRepository.DeleteAsync(tableId);
+        }
+
+        public async Task<List<Table>> GetAllAsync()
+        {
+            return await _tableRepository.GetAllAsync();
+        }
+
+        public async Task<Table> GetByIdAsync(int tableId)
+        {
+            return await _tableRepository.GetByIdAsync(tableId);
+        }
+
+        public async Task UpdateAsync(Table updatedTable)
+        {
+            await _tableRepository.UpdateAsync(updatedTable);
+        }
+    }
+}
