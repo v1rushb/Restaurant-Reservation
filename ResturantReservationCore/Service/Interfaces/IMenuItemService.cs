bed7818d191edcd60af4125879700c7789275d29
v1rushb <cs.bashar.herbawi@gmail.com>
2024-11-19 15:44:47 +0200

    Add Menu Item Service

--- ResturantReservationCore/Service/Interfaces/IMenuItemService.cs
+++ ResturantReservationCore/Service/Interfaces/IMenuItemService.cs
@@ -2,7 +2,7 @@ using RestaurantReservation.Db.Entities;
 
 namespace RestaurantReservationCore.Service.Interfaces
 {
-    public interface IMenuItem : IService<MenuItem>
+    public interface IMenuItemService : IService<MenuItem>
     {
 
     }
