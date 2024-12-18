e86f1eb59e2f4f9e27b63c17890f98657a0e6759
v1rushb <cs.bashar.herbawi@gmail.com>
2024-12-18 13:24:21 +0200

    Add Required Annotations for CustomerWithoutIdDTO

--- RestaurantReservation.API/Models/CustomerWithoutIdDTO.cs
+++ RestaurantReservation.API/Models/CustomerWithoutIdDTO.cs
@@ -1,11 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace RestaurantReservation.API.Models
 {
     public class CustomerWithoutIdDTO
-    {
+    {   
+        [Required]
         public string FirstName { get; set; }
+
+        [Required]
         public string LastName { get; set; }
-        
+
+        [Required]        
         public string Email { get; set; }
+
+        [Required]
         public string PhoneNumber { get; set; }
     }
 }
