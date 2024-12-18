24b76bb5051cc602bd6fd16fa8a7d33f1f3a497b
v1rushb <cs.bashar.herbawi@gmail.com>
2024-12-18 12:49:15 +0200

    Change signature of OrderService.GetAllAsync Method to return Meta data

--- RestaurantReservation.Db/Service/OrderService.cs
+++ RestaurantReservation.Db/Service/OrderService.cs
@@ -2,12 +2,16 @@ using System.Formats.Asn1;
 using RestaurantReservation.Db.Entities;
 using RestaurantReservation.Db.Repositories.interfaces;
 using RestaurantReservation.Db.Service.Interfaces;
+using RestaurantReservation.Db.Utilities;
+using RestaurantReservation.Db.Utilities.Models;
 
 namespace RestaurantReservation.Db.Service
 {
     public class OrderService : IOrderService
     {
         private readonly IOrderRepository _orderRepository;
+        private readonly PaginationMetadataGenerator<Order> _paginationMetadataGenerator = new();
+
 
         public OrderService(IOrderRepository orderRepository)
         {
@@ -23,9 +27,12 @@ namespace RestaurantReservation.Db.Service
             await _orderRepository.DeleteAsync(orderId);
         }
 
-        public async Task<List<Order>> GetAllAsync(int size, int pageSize)
+        public async Task<(List<Order>, Meta)> GetAllAsync(int page, int pageSize)
         {
-            return await _orderRepository.GetAllAsync(size, pageSize);
+            var orders = await _orderRepository.GetAllAsync(page, pageSize);
+            var metadata = _paginationMetadataGenerator.GetGeneratedMetadata(orders, page, pageSize);
+                
+            return (orders, metadata);
         }
 
         public async Task<Order> GetByIdAsync(int orderId)
