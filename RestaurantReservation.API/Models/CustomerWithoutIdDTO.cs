bd05f098c3f0325127c3499a8d876138b44e1c45
v1rushb <cs.bashar.herbawi@gmail.com>
2024-12-18 08:05:51 +0200

    Add CustomerWithoutIdDTO Model

---
+++ RestaurantReservation.API/Models/CustomerWithoutIdDTO.cs
@@ -0,0 +1,11 @@
+namespace RestaurantReservation.API.Models
+{
+    public class CustomerWithoutIdDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
