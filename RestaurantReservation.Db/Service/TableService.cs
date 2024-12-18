b10bccacae74e4dc95a356e838d5c638f630b1b9
v1rushb <cs.bashar.herbawi@gmail.com>
2024-12-18 13:02:00 +0200

    Change signature of TableService.GetAllAsync Method to return Meta data

--- RestaurantReservation.Db/Service/TableService.cs
+++ RestaurantReservation.Db/Service/TableService.cs
@@ -1,12 +1,16 @@
 using RestaurantReservation.Db.Entities;
 using RestaurantReservation.Db.Repositories.interfaces;
 using RestaurantReservation.Db.Service.Interfaces;
+using RestaurantReservation.Db.Utilities;
+using RestaurantReservation.Db.Utilities.Models;
 
 namespace RestaurantReservation.Db.Service
 {
     public class TableService : ITableService
     {
         private readonly ITableRepository _tableRepository;
+        private readonly PaginationMetadataGenerator<Table> _paginationMetadataGenerator = new();
+
 
         public TableService(ITableRepository tableRepository)
         {
@@ -22,9 +26,12 @@ namespace RestaurantReservation.Db.Service
             await _tableRepository.DeleteAsync(tableId);
         }
 
-        public async Task<List<Table>> GetAllAsync(int size, int pageSize)
+        public async Task<(List<Table>, Meta)> GetAllAsync(int page, int pageSize)
         {
-            return await _tableRepository.GetAllAsync(size, pageSize);
+            var tables = await _tableRepository.GetAllAsync(page, pageSize);
+            var metadata = _paginationMetadataGenerator.GetGeneratedMetadata(tables, page, pageSize);
+                
+            return (tables, metadata);
         }
 
         public async Task<Table> GetByIdAsync(int tableId)
