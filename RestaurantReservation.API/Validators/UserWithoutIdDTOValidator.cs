76b8b1337c6bff97877246b2c119ebff96ccc0c8
v1rushb <cs.bashar.herbawi@gmail.com>
2024-12-21 13:33:52 +0200

    Add UserRegistration Validator

---
+++ RestaurantReservation.API/Validators/UserWithoutIdDTOValidator.cs
@@ -0,0 +1,30 @@
+using FluentValidation;
+using RestaurantReservation.API.Constants;
+using RestaurantReservation.API.Models;
+using RestaurantReservation.Db.Entities;
+
+namespace RestaurantReservation.API.Validators
+{
+    public class UserWithoutDTOValidator : AbstractValidator<UserWithoutIdDTO>
+    {
+        public UserWithoutDTOValidator()
+        {
+
+            RuleFor(user => user.Username)
+                .Matches(RegexPatterns.Username)
+                .WithMessage(ErrorMessages.InvalidUsername)
+                .Length(Username.MinimumLength, Username.MinimumLength);
+
+            RuleFor(user => user.Password)
+                .Length(Password.MinimumLength, Password.MaximumLength);
+
+            RuleFor(user => user.FirstName)
+                .NotEmpty().WithMessage("First name is required.")
+                .MaximumLength(45).WithMessage("First name must not exceed 45 characters.");
+
+            RuleFor(user => user.LastName)
+                .NotEmpty().WithMessage("Last name is required.")
+                .MaximumLength(45).WithMessage("Last name must not exceed 45 characters.");
+        }
+    }
+}
