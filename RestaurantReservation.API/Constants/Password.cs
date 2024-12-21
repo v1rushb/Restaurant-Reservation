d511862e748aec88d0f612a33ecd94c9a8e5f7ab
v1rushb <cs.bashar.herbawi@gmail.com>
2024-12-21 13:34:14 +0200

    Add Username/Password Constants

---
+++ RestaurantReservation.API/Constants/Password.cs
@@ -0,0 +1,8 @@
+namespace RestaurantReservation.API.Constants
+{
+    public class Password
+    {
+        public const int MinimumLength = 8;
+        public const int MaximumLength = 45;
+    }
+}
---
+++ RestaurantReservation.API/Constants/Username.cs
@@ -0,0 +1,8 @@
+namespace RestaurantReservation.API.Constants
+{
+    public class Username
+    {
+        public const int MinimumLength = 2;
+        public const int MaximumLength = 45;
+    }
+}
