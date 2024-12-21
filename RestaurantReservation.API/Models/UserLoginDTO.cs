022c8386f5163b86e39bce894e558054fe4484ad
v1rushb <cs.bashar.herbawi@gmail.com>
2024-12-21 13:35:13 +0200

    Add UserLoginDTO Model

---
+++ RestaurantReservation.API/Models/UserLoginDTO.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantReservation.API.Models
+{
+    public class UserLoginDTO
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+    }
+}
