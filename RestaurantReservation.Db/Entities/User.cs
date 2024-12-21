3fe3202c3c230fab027ae87d7e22dbc148a1a9aa
v1rushb <cs.bashar.herbawi@gmail.com>
2024-12-21 11:56:05 +0200

    Introduce a user representation Entity

---
+++ RestaurantReservation.Db/Entities/User.cs
@@ -0,0 +1,11 @@
+namespace RestaurantReservation.Db.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string password { get; set; }
+    }
+}
